440afffd477b26ff41cafce067ec40a6ec18e86c
Ayush Jaiswal <aj85163@gmail.com>
2026-01-17 15:11:44 +0530

    convex deployment issue fixed

--- app/ConvexClientProvider.jsx
+++ app/ConvexClientProvider.jsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import React from "react";
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "https://kindly-hedgehog-391.convex.cloud");
+import React, { useMemo } from "react";
 
 function ConvexClientProvider({ children }) {
+  const convex = useMemo(() => {
+    const url = process.env.NEXT_PUBLIC_CONVEX_URL || "https://kindly-hedgehog-391.convex.cloud";
+    return new ConvexReactClient(url);
+  }, []);
+
   return <ConvexProvider client={convex}>{children}</ConvexProvider>
 }
 
